c71de092f847f34c8ccc40ea74a68bcc71fdfb44
Nick Murphy <nickodel4@gmail.com>
2025-04-18 10:58:43 +0200

    percolation
    percolation
    
    percolation v4
    
    percolation v5
    
    use virtual top and bottom
    
    two weighted quick find
    
    try again
    
    renames
    
    percolation 100%

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,14 +1,16 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
 
     private boolean[][] grid;
     private int gridDimension;
-    private WeightedQuickUnionUF currentConnections;
+    private WeightedQuickUnionUF currentConnectionsWithSink;
+    private WeightedQuickUnionUF currentConnectionsNoSink;
     private int openSites = 0;
+    private final static int TOP = 0;
+    private int bottom;
 
- // creates n-by-n grid, with all sites initially blocked
+    // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException(String.format("Invalid argument %1$s passed to constructor!, The percolation grid must have a strictly positive size", n));
@@ -20,40 +22,52 @@ public class Percolation {
                 grid[i][j] = false;
             }
         }
-        currentConnections = new WeightedQuickUnionUF(gridDimension * gridDimension);
+        currentConnectionsWithSink = new WeightedQuickUnionUF(gridDimension * gridDimension + 2);
+        currentConnectionsNoSink = new WeightedQuickUnionUF(gridDimension * gridDimension + 1);
+        bottom = gridDimension * gridDimension + 1;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         checkDimension(row);
         checkDimension(col);
-        if (!grid[row - 1][col - 1]) { 
+        if (!grid[row - 1][col - 1]) {
             grid[row - 1][col - 1] = true;
             openSites++;
+            int uniqueId = mapIndicesToUniqueId(row, col);
+            if (row == 1) {
+                currentConnectionsWithSink.union(uniqueId, TOP);
+                currentConnectionsNoSink.union(uniqueId, TOP);
+            }
+            if (row == gridDimension)
+                currentConnectionsWithSink.union(uniqueId, bottom);
+            if (col > 1 && isOpen(row, col - 1)) {
+                currentConnectionsWithSink.union(uniqueId, mapIndicesToUniqueId(row, col - 1));
+                currentConnectionsNoSink.union(uniqueId, mapIndicesToUniqueId(row, col - 1));
+            }
+            if (row > 1 && isOpen(row - 1, col)) {
+                currentConnectionsWithSink.union(uniqueId, mapIndicesToUniqueId(row - 1, col));
+                currentConnectionsNoSink.union(uniqueId, mapIndicesToUniqueId(row - 1, col));
+            }
+            if (row < gridDimension && isOpen(row + 1, col)) {
+                currentConnectionsWithSink.union(uniqueId, mapIndicesToUniqueId(row + 1, col));
+                currentConnectionsNoSink.union(uniqueId, mapIndicesToUniqueId(row + 1, col));
+            }
+            if (col < gridDimension && isOpen(row, col + 1)) {
+                currentConnectionsWithSink.union(uniqueId, mapIndicesToUniqueId(row, col + 1));
+                currentConnectionsNoSink.union(uniqueId, mapIndicesToUniqueId(row, col + 1));
+            }
         }
-        int uniqueId = mapIndicesToUniqueId(row, col);
-        if (col - 1 > 0 && isOpen(row, col - 1)) {
-            currentConnections.union(uniqueId, mapIndicesToUniqueId(row, col - 1));
-        }
-        if (row - 1 > 0 && isOpen(row - 1, col)) {
-            currentConnections.union(uniqueId, mapIndicesToUniqueId(row - 1 , col));
-        }     
-        if (row + 1 <= gridDimension && isOpen(row + 1, col)) {
-            currentConnections.union(uniqueId, mapIndicesToUniqueId(row + 1 , col));
-        }     
-        if (col + 1 <= gridDimension && isOpen(row, col + 1)) {
-            currentConnections.union(uniqueId, mapIndicesToUniqueId(row, col + 1));
-        }     
     }
 
     private int mapIndicesToUniqueId(int row, int col) {
         checkDimension(row);
         checkDimension(col);
-        return gridDimension * (row - 1) + (col - 1);
+        return gridDimension * (row - 1) + col;
     }
     private void checkDimension(int dim) {
         if (dim <= 0 || dim > gridDimension)
-            throw new IllegalArgumentException(String.format("Invalid dimension %1$s!, Grid dimensions must be positive and <= %2$s", dim, gridDimension)); 
+            throw new IllegalArgumentException(String.format("Invalid dimension %1$s!, Grid dimensions must be positive and <= %2$s", dim, gridDimension));
     }
 
 
@@ -66,31 +80,16 @@ public class Percolation {
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        int index = mapIndicesToUniqueId(row, col);
-        boolean status = false;
-        int j = 1;
-        while (j <= gridDimension) {
-            status = currentConnections.find(mapIndicesToUniqueId(1, j)) == currentConnections.find(index);
-            j++;
-            if (status)
-                break;
-        }
-        return status;
+        return currentConnectionsNoSink.find(TOP) == currentConnectionsNoSink.find(mapIndicesToUniqueId(row, col));
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return openSites; 
+        return openSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        boolean p = false;
-        int i = 1;
-        while (!p && i <= gridDimension) {
-            p = isFull(gridDimension, i);
-            i++;
-        }
-        return p;
+        return currentConnectionsWithSink.find(TOP) == currentConnectionsWithSink.find(bottom);
     }
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,19 +1,22 @@
-import java.lang.Math;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
+    private static final double CONFIDENCE_95 = 1.96;
     private double[] trialResults;
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        if (trials < 2)
-            throw new IllegalArgumentException(String.format("You must run at least two trials to create a PercolationStats object, trials = %1$s", trials));
-
+        if (n <= 0) 
+            throw new IllegalArgumentException("Dimensions of grid must be strictly positive");
+        if (trials <= 0) 
+            throw new IllegalArgumentException("Number of trials must be strictly positive");
         double percolationThreshold = 0;
         trialResults = new double[trials];
-        for (int i = 0; i < trials; i++)
+        for (int i = 0; i < trials; i++) {
             trialResults[i] = performExperiment(n);
+        }
     }
 
     private void openRandom(int upperBoundExclusive, Percolation p) {
@@ -25,44 +28,34 @@ public class PercolationStats {
     private double performExperiment(int n) {
         Percolation percolation = new Percolation(n);
         for (int i = 0; i < n; i++)
-            openRandom(n, percolation);    
+            openRandom(n, percolation);
 
         while (!percolation.percolates())
             openRandom(n, percolation);
-        
-        double percolationThreshold = (double)percolation.numberOfOpenSites() / (n*n);
+
+        double percolationThreshold = (double) percolation.numberOfOpenSites() / (n * n);
         return percolationThreshold;
-        
+
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        double sum = 0.0; // initialize sum
-        for (int i = 0; i < trialResults.length; i++)
-            sum += trialResults[i];
-        return sum / trialResults.length;
+        return StdStats.mean(trialResults);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        double sum = 0.0;
-        double diff;
-        for (int i = 0; i < trialResults.length; i++) {
-            diff = trialResults[i] - mean();
-            sum += diff * diff;
-        }
-
-        return sum / (trialResults.length - 1);
+        return StdStats.stddev(trialResults);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - 1.96 * stddev() / Math.sqrt(trialResults.length);
+        return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(trialResults.length);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + 1.96 * stddev() / Math.sqrt(trialResults.length);
+        return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(trialResults.length);
     }
 
     // test client (see below)
@@ -70,8 +63,8 @@ public class PercolationStats {
         int dimension = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
         PercolationStats percolationStats = new PercolationStats(dimension, trials);
-        StdOut.printf("mean                    = %.20f\n", percolationStats.mean());
-        StdOut.printf("stddev                  = %.20f\n", percolationStats.stddev());
+        StdOut.printf("mean                     = %.20f\n", percolationStats.mean());
+        StdOut.printf("stddev                   = %.20f\n", percolationStats.stddev());
         StdOut.printf("95%% confidence interval = [%1$.20g, %2$.20g]\n", percolationStats.confidenceLo(), percolationStats.confidenceHi());
     }
 
